f1b27ffa9cd981b92a9c675735db61ef546b05a7
graziabaiamonte <graziabaiamonte19@gmail.com>
2025-10-14 15:50:55 +0200

    aggiunto screen per readme

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,5 @@
+// per funzionamento in locale
 export const API_URL = "http://localhost:3000/api";
+
+// per funzionamento in prod
+// export const API_URL = "https://bookstore-reactnative.onrender.com";
